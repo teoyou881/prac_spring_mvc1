629d41bb2dc0dbdc7746bc59885fd6fcacb5cec7
Hyungchul You <teoyou881@gmail.com>
2023-10-07 01:07:29 -0400

    Build process to register product

--- src/main/java/prac_spring_mvc1/demo/domain/web/basic/BasicItemController.java
+++ src/main/java/prac_spring_mvc1/demo/domain/web/basic/BasicItemController.java
@@ -6,9 +6,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import prac_spring_mvc1.demo.domain.item.Item;
 import prac_spring_mvc1.demo.domain.item.ItemRepository;
 
@@ -19,8 +21,7 @@ public class BasicItemController {
     
     private final ItemRepository itemRepository;
     
-    @GetMapping
-    public String items(Model model) {
+    @GetMapping public String items(Model model) {
         List<Item> items = itemRepository.findAll();
         model.addAttribute("items", items);
         return "basic/items";
@@ -29,26 +30,61 @@ public class BasicItemController {
     @GetMapping("/{itemId}")
     public String item(
         @PathVariable
-        Long itemId, Model model
-    ) {
+        Long itemId, Model model) {
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item", item);
         return "basic/item";
     }
     
-    @GetMapping("/add")
-    public String addForm() {
+    @GetMapping("/add") public String addForm() {
         return "basic/addForm";
+    }
+    
+    //    @PostMapping("/add")
+    public String addItemV1(
+        @RequestParam
+        String itemName,
+        @RequestParam
+        int price,
+        @RequestParam
+        Integer quantity, Model model) {
+        
+        Item item = new Item(itemName, price, quantity);
+        
+        itemRepository.save(item);
+        
+        model.addAttribute("item", item);
+        
+        return "basic/item";
+    }
+    
+    //    @PostMapping("/add")
+    public String addItemV2(
+        // @ModelAttribute
+        // 1. create an object and bind it to the passed parameters.
+        // 2. do model.addAttribute(); automatically.
+        /*ModelAttribute("hello") =>  model.add~~("heelo", item)
+         *If there is no name like ModelAttribute =>
+         * Only the first leading character of the class name that follows is changed to lowercase.
+         *  @ModelAttribute HelloData item  -->  model.add~~("helloData", item)*/
+        @ModelAttribute
+        Item item
+    ) {
+        itemRepository.save(item);
+        return "basic/item";
     }
     
     @PostMapping("/add")
-    public String save() {
-        return "basic/addForm";
+    public String addItemV3(
+        // Can omit @ModelAttribute
+        Item item
+    ) {
+        itemRepository.save(item);
+        return "basic/item";
     }
     
     //data for test
-    @PostConstruct
-    public void init() {
+    @PostConstruct public void init() {
         itemRepository.save(new Item("A", 1, 100));
         itemRepository.save(new Item("B", 2, 200));
     }
