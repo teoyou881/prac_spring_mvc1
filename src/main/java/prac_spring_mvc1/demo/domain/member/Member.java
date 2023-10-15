4ab9b3b62866ce8e74a36b9247b8dd3e05022140
Hyungchul You <teoyou881@gmail.com>
2023-10-15 11:12:06 -0400

    Build Member domain like Member, MemberRepository

---
+++ src/main/java/prac_spring_mvc1/demo/domain/member/Member.java
@@ -0,0 +1,16 @@
+package prac_spring_mvc1.demo.domain.member;
+
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Data;
+
+@Data
+public class Member {
+
+	private Long id;
+	@NotEmpty
+	private String loginId;
+	@NotEmpty
+	private String name;
+	@NotEmpty
+	private String password;
+}
